291628c6b6b1fcadba9852b3f84cdb8c79333ef0
Josh Mineroff <jmineroff@gmail.com>
2022-05-07 23:49:20 -0700

    Create launchpad-init.sh

---
+++ scripts/launchpad-init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp -a /repos/Domino-Launchpad-Quickstart /mnt
+
+sed -i -e 's|<img src = |![image](raw/latest/|g' /mnt/README.md
+sed -i -e 's| width="800">|)' /mnt/README.md
+
+sed '|<p align="center">|d' /mnt/README.md
+sed '|</p>|d' /mnt/README.md
