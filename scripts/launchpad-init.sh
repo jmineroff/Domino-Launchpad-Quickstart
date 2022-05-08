ed771304137e6cc4bbb74a2f6c346d2bfbdd0157
Josh Mineroff <jmineroff@gmail.com>
2022-05-08 00:07:25 -0700

    Update launchpad-init.sh

--- scripts/launchpad-init.sh
+++ scripts/launchpad-init.sh
@@ -5,5 +5,5 @@ cp -a /repos/Domino-Launchpad-Quickstart/* /mnt
 sed -i -e 's|<img src = |![image](raw/latest/|g' /mnt/README.md
 sed -i -e 's| width="800">|)|g' /mnt/README.md
 
-sed '|<p align="center">|d' /mnt/README.md
-sed '|</p>|d' /mnt/README.md
+sed '\|<p align="center">|d' /mnt/README.md
+sed '\|</p>|d' /mnt/README.md
